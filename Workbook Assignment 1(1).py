31765e4b1861ba5e79b76fed62b4bf11c9f04480
timothyd1 <52899859+timothyd1@users.noreply.github.com>
2020-08-31 18:14:16 -0400

    Add files via upload

---
+++ Workbook Assignment 1(1).py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+import pandas as pd
+
+
+# In[10]:
+
+
+cd C:/Users/Timothy Davis/Desktop/ISM 4402
+
+
+# In[12]:
+
+
+pd.read_csv("all_040_in_39.P1.csv")
+
+
+# In[ ]:
+
+
+
+
