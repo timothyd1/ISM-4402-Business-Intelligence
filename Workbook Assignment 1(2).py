6af64cd13e16df488df0fdec487e31319d3c3c41
timothyd1 <52899859+timothyd1@users.noreply.github.com>
2020-08-31 18:28:46 -0400

    Add files via upload

---
+++ Workbook Assignment 1(2).py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+import pandas as pd
+
+
+# In[3]:
+
+
+cd C:/Users/Timothy Davis/Desktop/ISM 4402
+
+
+# In[8]:
+
+
+df = pd.read_excel("RHL01.xls")
+
+
+# In[9]:
+
+
+df.head()
+
+
+# In[ ]:
+
+
+
+
