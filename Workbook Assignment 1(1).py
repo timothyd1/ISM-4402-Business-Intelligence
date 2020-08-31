cd796455a7c56f52181f38e35697f7465f080f57
timothyd1 <52899859+timothyd1@users.noreply.github.com>
2020-08-31 18:25:00 -0400

    Add files via upload

--- Workbook Assignment 1(1).py
+++ Workbook Assignment 1(1).py
@@ -13,10 +13,16 @@ import pandas as pd
 cd C:/Users/Timothy Davis/Desktop/ISM 4402
 
 
-# In[12]:
+# In[17]:
 
 
-pd.read_csv("all_040_in_39.P1.csv")
+df = pd.read_csv("all_040_in_39.P1.csv")
+
+
+# In[19]:
+
+
+df.head()
 
 
 # In[ ]:
